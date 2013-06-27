f5d720c7c391426dd28d03943898d7a7d2cd79c0
Rob Scanlon <robscanlon@gmail.com>
2013-06-27 23:25:46 +0000

    daily updates

--- lib/glance/card.rb
+++ lib/glance/card.rb
@@ -2,9 +2,18 @@ module Glance
   class Card
 
     attr_reader :question, :answer, :difficulty
+    attr_reader :history, :last, :unlocked
 
-    def initialize question, answer, difficulty, opts = {}
+    def initialize(question, answer, difficulty, opts = {})
       @question, @answer, @difficulty = question, answer, difficulty
+
+      @history = []
+      @last = Time.now
+      @unlocked  = false
+
+      @history = opts[:history] if opts.has_key?(:history)
+      @last = opts[:last] if opts.has_key?(:last)
+      @unlocked = opts[:unlocked] if opts.has_key?(:unlocked)
       
     end
   end
