870183671ad743b8dca66d45545a87158a047f70
Rob Scanlon <robscanlon@gmail.com>
2013-06-28 22:18:30 +0000

    add more smarts to cards

--- lib/glance/card.rb
+++ lib/glance/card.rb
@@ -2,19 +2,56 @@ module Glance
   class Card
 
     attr_reader :question, :answer, :difficulty
-    attr_reader :history, :last, :unlocked
+    attr_reader :history, :next, :last, :unlocked
 
     def initialize(question, answer, difficulty, opts = {})
       @question, @answer, @difficulty = question, answer, difficulty
 
       @history = []
+      @next = Time.now
       @last = Time.now
       @unlocked  = false
 
       @history = opts[:history] if opts.has_key?(:history)
       @last = opts[:last] if opts.has_key?(:last)
+      @next = opts[:next] if opts.has_key?(:next)
       @unlocked = opts[:unlocked] if opts.has_key?(:unlocked)
       
     end
+
+    def score(score)
+      @history << score
+      @next = calc_next
+      @last = Time.now
+      return false if history.length < 2
+      history[-2..-1].all?{|h| h == 3}
+    end
+
+    private
+
+    def calc_next
+
+      # count the number of perfect scores at the end
+      first_two = @history.reverse.index(2)
+      first_one = @history.reverse.index(1)
+
+      first_two ||= @history.length
+      first_one ||= @history.length
+
+      non_three = [first_one, first_two].min
+
+      # count the number of threes since my last one
+
+      recent_threes = history.reverse[0..first_one].count(3)
+
+      # if had a one in the last 5, automatically keep me around
+      #
+      return Time.now if first_one < 5
+
+      Time.now + recent_threes * 60 * 60 * 24
+
+    end
+
   end
+
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -28,9 +28,91 @@ module Glance
       @card.difficulty.should eql 1
     end
 
-    it "hsitory should be an array of lenght 6" do
+    it "should have a history length 6" do
       @card.history.length.should eql 6
     end
+
+    it "should have a history length 0 when history not supplied" do
+      Card.new("questsion","answer",1).history.length.should eql 0
+    end
+
+    it "shouldn't be unlocked by default" do
+      @card.unlocked.should eql false
+    end
+
+    describe "#score" do
+      before :each do
+        @card = Card.new "Question", "Answer", 1
+      end
+
+      it "should return true if I score a couple 3's in a row" do
+        @card.score(2)
+        @card.score(1)
+        @card.score(3)
+        @card.score(3).should eq true
+        
+      end
+
+      it "should return false if I don't score a couple 3's in a row" do
+        @card.score(3)
+        @card.score(2)
+        @card.score(1)
+        @card.score(3).should eq false
+      end
+
+      it "should return false the fist score that I do" do
+        @card.score(3).should eq false
+      end
+
+      it "should say that i went recently" do
+        @card.score(3)
+        @card.last.should be < Time.now
+        @card.last.should be > Time.now - 10
+
+      end
+
+      it "should tell me to go again if I had a one recently" do
+        3.times {@card.score(2)}
+        @card.score(1)
+        3.times {@card.score(2)}
+        @card.score(3)
+        @card.next.should be < Time.now
+      end
+
+      it "should tell me I don't need to go again if I've been doing well" do
+        @card.score(1)
+        5.times {@card.score(3)}
+        2.times {@card.score(2)}
+        2.times {@card.score(3)}
+        @card.next.should be > Time.now
+      end
+
+      it "should not make me do this for a few more days" do
+        5.times {@card.score(3)}
+        @card.next.should be > Time.now + (60*60*24) * 3
+        @card.next.should be < Time.now + (60*60*24)*6
+      end
+
+      it "should not make me do this for a few more days" do
+        5.times {@card.score(3)}
+        @card.score(1)
+        5.times {@card.score(3)}
+        @card.next.should be > Time.now + (60*60*24) * 3
+        @card.next.should be < Time.now + (60*60*24)*6
+      end
+
+      it "should not make me do this for a few more days" do
+        5.times {@card.score(3)}
+        @card.score(1)
+        5.times {@card.score(3)}
+        2.times {@card.score(2)}
+        2.times {@card.score(3)}
+        @card.next.should be > Time.now + (60*60*24) * 6
+        @card.next.should be < Time.now + (60*60*24)*8
+      end
+
+    end
+
   end
 
 end
